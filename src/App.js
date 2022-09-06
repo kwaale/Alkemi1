42725a3d56d2bf37d717d4b11e41e3562e3f66a2
Knut Waale <kwaale@github.com>
2022-09-06 16:56:10 -0300

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import Login from './components/Login'
+import Login from './components/Login';
+import Listado from './components/Listado';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-      <h2>
-        App
-      </h2>
-      <Login />
-    </div>
+    
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/listado' element={<Listado/>}/>
+      </Routes>
+    
   );
 }
 
