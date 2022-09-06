daf58053df4d77986b6744b4b97698af5cc38eaa
Knut Waale <kwaale@github.com>
2022-09-06 17:54:58 -0300

    add footer-header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Login from './components/Login';
 import Listado from './components/Listado';
 import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    
+    <>
+      <Header />
       <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/listado' element={<Listado/>}/>
+        <Route path='/' element={<Login />} />
+        <Route path='/listado' element={<Listado />} />
       </Routes>
-    
+      <Footer />
+    </>
+
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return(
+        <footer>
+            <nav>
+                <ul><a href='https://github.com/kwaale' rel='noopener noreferrer'>Github</a></ul>
+            </nav>
+            <p>Copyright Knut Waale</p>
+        </footer>
+    )
+}
+export default Footer;
