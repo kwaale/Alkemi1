8212a475410ca4c8fe9ea4cb684266df3d44b897
Knut Waale <kwaale@github.com>
2022-09-15 07:19:29 -0300

    Link Movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,9 @@
-const Movie = ({name, premiere, language, poster, overview}) => {
+import { Link } from 'react-router-dom';
+
+const Movie = ({id, name, premiere, language, poster, overview}) => {
+    
     return (
-        <div className="card mb-3" style={{"max-width" : "540px"}}>
+        <div className="card mb-3" style={{"maxWidth" : "540px"}}>
             <div className="row g-0">
                 <div className="col-md-4">
                     <img src={poster} className="img-fluid rounded-start" alt={name} />
@@ -8,9 +11,10 @@ const Movie = ({name, premiere, language, poster, overview}) => {
                 <div className="col-md-8">
                     <div className="card-body">
                         <h5 className="card-title">{name}</h5>
-                        <p className="card-text">{overview}</p>
+                        <p className="card-text">{overview.substring(0,250) + "..."}</p>
                         <p className="card-text">Language: {language}</p>
                         <p className="card-text"><small className="text-muted">Premiere: {premiere}</small></p>
+                        <Link to={`/detalle?id=${id}`} class="btn btn-primary">Details</Link>
                     </div>
                 </div>
             </div>
