4302d5b67d9705181890bc3bff6ff52d92347225
Knut Waale <kwaale@github.com>
2022-09-15 15:48:15 -0300

    Agregando detalle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Listado from './components/Listado';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MovieDetail from './components/MovieDetails';
 
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/listado' element={<Listado />} />
+          <Route path='/detalle' element={<MovieDetail />} />
+          {/* <Route path='/detalle1' element={<MovieDetail />} /> */}
         </Routes>
       </div>
       <Footer />
