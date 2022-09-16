39e43bdac89ff9e7c5070e90aab38707f21c3bb2
Knut Waale <kwaale@github.com>
2022-09-16 10:09:45 -0300

    Cargando...

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -10,21 +10,23 @@ const MovieDetail = () => {
     const token = sessionStorage.getItem('token');
     let query = new URLSearchParams(window.location.search)
     const id = query.get('movieID');
-
+    //https://api.themoviedb.org/3/movie/63174?api_key=9f3d130990d75b5f5409f458cb910a4b&language=us-US
+    //https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=es-ES
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=us-US`)
+        axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-ES`)
             .then(res => {
                 setState(res.data)
-                console.log(res.data)
+                console.log(res.data.original_title)
             })
             .catch(error => console.error(error));
     }, [id]);
 
     return (
         <>
+            {!state && <h1>Cargando</h1>}
             {!token ? (<Navigate replace to="/"/>) :
-            (<div className="card mb-3">
-                <h1>Id: {id}</h1>
+            (state && <div className="card mb-3">
+                <h1>Id: {state.id}</h1>
                 <img src={URL_IMAGE + state.poster_path} className="card-img-top" alt={state.original_title} />
                 <div className="card-body">
                     <h5 className="card-title">{state.original_title}</h5>
